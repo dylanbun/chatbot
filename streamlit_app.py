21c873158b79f7316033e402d812cd555dd06281
Dylan <dylandb2020@gmail.com>
2024-08-18 20:15:13 -0700

    added sheets

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 from openai import OpenAI
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
 
 # Show title and description.
 st.title("💬 Chatbot")
